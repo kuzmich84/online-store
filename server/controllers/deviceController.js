ef76a2672a544b48525e6ba481f61043ddb5bfff
Kuchukov Dmitry <kudim@inbox.ru>
2022-05-24 15:15:38 +0300

    create controllers

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,17 @@
+class DeviceController {
+    async create(req, res) {
+
+    }
+
+    async getAll(req, res) {
+        res.json('getAllDevice')
+
+    }
+
+    async getOne(req, res) {
+
+    }
+
+}
+
+module.exports = new DeviceController()
